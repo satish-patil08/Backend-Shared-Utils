5b203d701348ea5ef450105c0c0034077c6f0253
Satish Patil <syapatil0803@gmail.com>
2025-01-17 15:21:35 +0530

    .

---
+++ src/test/java/com/microservices/shared_utils/SharedUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.shared_utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
