ab1cc001f4abb52c169bc4bb4a77608e99b1ea9d
Satish Patil <syapatil0803@gmail.com>
2025-01-21 11:36:46 +0530

    .

--- src/main/java/com/microservices/shared_utils/constants/Constants.java
+++ src/main/java/com/microservices/shared_utils/constants/Constants.java
@@ -2,4 +2,6 @@ package com.microservices.shared_utils.constants;
 
 public class Constants {
     public static final String ERROR_MESSAGE = "Sry. Please try after sometime";
+    public static final String SAMPLE_ERROR_MSG = "Sry. Please try after sometime";
+
 }
